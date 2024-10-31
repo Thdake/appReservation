fd454d2dbda62bc4c09154cf45f4d4381ad2a320
Dany Huaman� Chac�n <dany.huch@gmail.com>
2024-10-31 14:43:33 -0500

    Proyect

---
+++ src/main/java/com/example/demo/Pruea.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/saludar")
+
+public class Pruea {
+    @GetMapping ("/prueba")
+    public String Saludar(){
+        return "Hola";
+    }
+}
